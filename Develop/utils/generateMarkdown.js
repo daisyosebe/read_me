96b152a2d2b2d41e0f2c51a5fdc2c50d012461e7
daisyosebe <145105156+daisyosebe@users.noreply.github.com>
2024-05-31 16:56:27 -0500

    renderlicenselink

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -16,7 +16,20 @@ function renderLicenseBadge(license) {
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (!license) {
+      return '';
+  }
+
+  const links = {
+      'MIT': 'This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details',
+      'GPLv3': 'This project is licensed under the GPLv3 License - see the [LICENSE](LICENSE) file for details',
+      'Apache 2.0': 'This project is licensed under the Apache License 2.0 - see the [LICENSE](LICENSE) file for details'
+  };
+
+  return links[license] || '';
+}
+
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
