2508356af714d5fc323d910691ad9ddf489512e1
daisyosebe <145105156+daisyosebe@users.noreply.github.com>
2024-05-31 16:55:07 -0500

    renderlicensebadge code

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,6 +1,18 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (!license) {
+      return '';
+  }
+
+  const badges = {
+      'MIT': '![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)',
+      'GPLv3': '![License: GPLv3](https://img.shields.io/badge/License-GPLv3-blue.svg)',
+      'Apache 2.0': '![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)'
+  };
+
+  return badges[license] || '';
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
